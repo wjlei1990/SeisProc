900ab94f6e1f7294588377e0424d4f52ecb6d249
lei@titan <lei@princeton.edu>
2015-02-05 23:33:15 -0500

    Modification to process script(serial version) to make it more easy to use

--- src/preproc_asdf/create_obsd_asdf_file.py
+++ src/preproc_asdf/create_obsd_asdf_file.py
@@ -4,14 +4,24 @@ from convert_util import convert_asdf
 
 eventname = "010202E"
 
-basedir = "/lustre/atlas/proj-shared/geo111/Wenjie/DATA_PROCESS/OBSD/waveforms"
+basedir = "/lustre/atlas/proj-shared/geo111/Wenjie/DATA_PROCESS/OBSD"
 filetype = "mseed"
 
-output_fn = eventname + "_obsd.h5"
+tag = "raw_recording"
 
-datadir = os.path.join(basedir, eventname)
+output_fn = eventname + "_" + tag  + ".h5"
+
+# data list
+datadir = os.path.join(basedir, "waveforms", eventname)
 search_pattern = os.path.join(datadir, "*"+filetype)
 filelist = glob.glob(search_pattern)
+
+#stationxml
+staxmldir = os.path.join(basedir, "stationxml", eventname)
+search_pattern = os.path.join(staxmldir, "*.xml")
+staxml_list = glob.glob(search_pattern)
+print "SP:", search_pattern
+print "Number of Statoinxml found:", len(staxml_list)
 
 print "="*20
 print "Search Pattern:", search_pattern
@@ -22,5 +32,5 @@ if os.path.exists(output_fn):
     print "Outfile exist and being removed:", output_fn
     os.remove(output_fn)
 
-if len(filelist) >= 0:
-    convert_asdf(filelist, output_fn)
+#if len(filelist) >= 0:
+#    convert_asdf(filelist, output_fn, stationxml_filelist, tag=tag)
