4213cb7b9bb2bff014103c59ebd61e9b4412e938
lei <lei@princeton.edu>
2015-02-16 15:24:49 -0500

    Modify the parallel_pyflex script but the script freeze somehow

--- src/preproc_asdf/parallel_pyflex.py
+++ src/preproc_asdf/parallel_pyflex.py
@@ -1,11 +1,22 @@
 import pyflex
 from pyasdf import ASDFDataSet
+import os
+import glob
+import time
 
-ds = ASDFDataSet("./preprocessed_27s_to_60s.h5")
-other_ds = ASDFDataSet("./preprocessed_synthetic_27s_to_60s.h5")
+eventname = "201302091416A"
+obsd_fn = eventname + "_proc_27s_to_60s.h5"
+synt_fn = eventname + "_proc_synt_27s_to_60s.h5"
 
-event = ds.events[0]
+if not os.path.exists(obsd_fn):
+    raise ValueError("No obsd file: %s" %obsd_fn)
+if not os.path.exists(synt_fn):
+    raise ValueError("No synt file: %s" %synt_fn)
 
+obsd_ds = ASDFDataSet(obsd_fn)
+synt_ds = ASDFDataSet(synt_fn)
+
+event = obsd_ds.events[0]
 
 def weight_function(win):
     return win.max_cc_value
@@ -21,21 +32,19 @@ config = pyflex.Config(
     s2n_limit=0.5,
     max_time_before_first_arrival=-50.0,
     min_surface_wave_velocity=3.0,
-    window_signal_to_noise_type="energy",
-    window_weight_fct=weight_function)
+    window_signal_to_noise_type="amplitude")
 
-
-def process(this_station_group, other_station_group):
+def window_func(this_station_group, other_station_group):
     # Make sure everything thats required is there.
     if not hasattr(this_station_group, "StationXML") or \
-            not hasattr(this_station_group, "preprocessed_27s_to_60s") or \
+            not hasattr(this_station_group, "proc_27s_to_60s") or \
             not hasattr(other_station_group,
-                        "preprocessed_synthetic_27s_to_60s"):
+                        "proc_synt_27s_to_60s"):
         return
 
     stationxml = this_station_group.StationXML
-    observed = this_station_group.preprocessed_27s_to_60s
-    synthetic = other_station_group.preprocessed_synthetic_27s_to_60s
+    observed = this_station_group.proc_27s_to_60s
+    synthetic = other_station_group.proc_synt_27s_to_60s
 
     all_windows = []
 
@@ -45,22 +54,30 @@ def process(this_station_group, other_station_group):
         if not obs or not syn:
             continue
 
-        windows = pyflex.select_windows(
-            obs, syn, config, event=event, station=stationxml)
+        ws = pyflex.WindowSelector(obs, syn, config, event=event,
+                station=stationxml)
+        windows = ws.select_windows()
         print("Station %s.%s component %s picked %i windows" % (
             stationxml[0].code, stationxml[0][0].code, component,
             len(windows)))
         if not windows:
             continue
         all_windows.append(windows)
+        #windows = pyflex.select_windows(
+        #    obs, syn, config, event=event, station=stationxml)
+        #print("Station %s.%s component %s picked %i windows" % (
+        #    stationxml[0].code, stationxml[0][0].code, component,
+        #    len(windows)))
+        #if not windows:
+        #    continue
+        #all_windows.append(windows)
     return all_windows
 
-import time
 a = time.time()
-results = ds.process_two_files_without_parallel_output(other_ds, process)
+results = obsd_ds.process_two_files_without_parallel_output(synt_ds, window_func)
 b = time.time()
 
-if ds.mpi.rank == 0:
+if obsd_ds.mpi.rank == 0:
     print(results)
     print len(results)
 
