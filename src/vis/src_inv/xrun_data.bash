132b1f7a91720c86723438703a6dd6a6657341dc
lei <lei@princeton.edu>
2015-02-27 00:12:37 -0500

    add plotting script

--- src/vis/src_inv/xrun_data.bash
+++ src/vis/src_inv/xrun_data.bash
@@ -5,7 +5,11 @@ scriptname="plot_source_inv.py"
 while read line
 do
 	echo "event:" $line
-	echo "python $scriptname $line"
-	python $scriptname $line
+	echo "python $scriptname $line ALL"
+	python $scriptname $line ALL
+	echo "python $scriptname $line BW"
+	python $scriptname $line BW
+	echo "python $scriptname $line SW"
+	python $scriptname $line SW
 done < XEVENTID
 
